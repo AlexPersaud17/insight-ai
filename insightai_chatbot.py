2c452f33760e6f41ba360d362549c65a9f9ab472
Alex Persaud <alrpersaud17@gmail.com>
2025-03-21 13:13:23 -0400

    new scrapes, progress bars

--- insightai_chatbot.py
+++ insightai_chatbot.py
@@ -11,7 +11,7 @@ def page_init():
     st.title("InsightAI")
 
 
-@st.cache_resource
+@st.cache_resource(show_spinner=False)
 def initialize_pinecone_index():
     pc = Pinecone(api_key = st.secrets["PINECONE_API_KEY"])
     index_name="insight-ai-support-bot"
@@ -30,27 +30,26 @@ def initialize_pinecone_index():
     index = pc.Index(index_name)
     return model, index
 
-@st.cache_resource
+@st.cache_resource(show_spinner=False)
 def help_center_scrape():
+    count=0
+    progress_bar = st.progress(count, "Scraping documentation...")
     urls_to_scrape = -1
     raw_data = []
     sitemap_url = "https://help.adjust.com/sitemap.xml"
     response = requests.get(sitemap_url)
     soup = BeautifulSoup(response.content, "xml")
     urls_and_lastmod = [(loc.text, loc.find_next("lastmod").text if loc.find_next("lastmod") else None) for loc in soup.find_all("loc")[:urls_to_scrape]]
-    # st.write("Data scraped:")
-    count=0
-    progress_bar = st.progress(count, "Scraping documentation...")
+
     for url, lastmod in urls_and_lastmod:
         count += 1
         page_response = requests.get(url)
         page_soup = BeautifulSoup(page_response.content, "html.parser")
         title = page_soup.title.string if page_soup.title else "No title found"
-        paragraphs = page_soup.find_all("p")
-        content = "\n".join(paragraph.text for paragraph in paragraphs[1:-1])
+        content = page_soup.get_text()
+        # content = "\n".join(paragraph.text for paragraph in paragraphs[1:-1])
         unique_id = str(url) + "_" + (lastmod if lastmod else "no_lastmod")
 
-        st.write(f"[{title}]({url})")
 
         raw_data.append({
             "url": url,
@@ -58,17 +57,19 @@ def help_center_scrape():
             "content": content,
             "unique_id": unique_id
         })
-        progress_bar.progress(count/len(urls_and_lastmod))
+        progress_bar.progress(count/len(urls_and_lastmod), "Scraping documentation...")
 
     data = pd.DataFrame(raw_data)
     st.write("Scraping complete.")
     return data
 
-@st.cache_resource
+@st.cache_resource(show_spinner=False)
 def create_chunk_df(data, _model):
+    progress_bar = st.progress(0, "Chunking and embedding...")
     chunk_size=256
     chunked_rows = []
-    for _, row in data.iterrows():
+    for i, row in data.iterrows():
+        progress_bar.progress(i/len(data), "Chunking and embedding...")
         chunks = [row["content"][i:i + chunk_size] for i in range(0, len(row["content"]), chunk_size)]
         for i, chunk in enumerate(chunks):
             chunked_rows.append({
@@ -77,6 +78,7 @@ def create_chunk_df(data, _model):
                 "url": row["url"],
                 "content": chunk
             })
+
     chunked_df = pd.DataFrame(chunked_rows)
     chunked_df["embedding"] = chunked_df.apply(create_embeddings, axis = 1, args=(_model,))
     st.write("Chunking and embedding complete.")
@@ -87,6 +89,7 @@ def create_embeddings(row, model):
     return model.encode(combined_text, show_progress_bar = False)
 
 def upsert_embeddings(data, index):
+    progress_bar = st.progress(0, "Initializing vectors...")
     batch_size = 100
     to_upsert = []
 
@@ -94,8 +97,11 @@ def upsert_embeddings(data, index):
         vector_exists = index.fetch(row["unique_id"]).get("vectors")
         if not vector_exists:
             to_upsert.append((row["unique_id"], row["embedding"], {"title": row["title"], "url": row["url"], "text": row["content"]}))
+        progress_bar.progress(i/len(data), "Initializing vectors...")
+    progress_bar = st.progress(0, "Upserting vectors...")
     for i in range(0, len(to_upsert), batch_size):
         index.upsert(vectors=to_upsert[i : i + batch_size])
+        progress_bar.progress(i/len(to_upsert), "Upserting vectors...")
     st.write("Upserting complete.")
 
 
@@ -124,7 +130,7 @@ def llm_response(client):
         model="gpt-4o-mini",
         messages=st.session_state.messages,
         temperature=0.0,
-        max_tokens=500,
+        max_tokens=5000,
         stream = True
     )
     response_text = st.write_stream(response) 
@@ -164,7 +170,8 @@ Stay patient and be respectful, even when the users question may seem unclear or
 
     for message in st.session_state.messages[1:]:
         with st.chat_message(message["role"]):
-            st.write(message["content"])
+            if "SYSTEMCONTEXT" not in message["content"]:
+                st.write(message["content"])
 
     if prompt := st.chat_input("How can I help?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -180,8 +187,8 @@ Stay patient and be respectful, even when the users question may seem unclear or
                     if relevant_docs:
                         formatted_context = format_context(relevant_docs)
 
-                        st.session_state.messages.append({"role": "user", 
-                                                          "content": f"Context:\n{formatted_context}\n\nQuestion: {prompt}"})
+                        st.session_state.messages.append({"role": "user",
+                                                          "content": f"SYSTEMCONTEXTContext:\n{formatted_context}\n\nQuestion: {prompt}"})
 
                         response_text = llm_response(client)
 
@@ -200,9 +207,16 @@ Stay patient and be respectful, even when the users question may seem unclear or
 
 def main():
     model, index = initialize_pinecone_index()
-    data = help_center_scrape()
-    data = create_chunk_df(data, model)
-    upsert_embeddings(data, index)
+    if "data" not in st.session_state:
+        st.session_state.data = help_center_scrape()
+
+    if "chunked_data" not in st.session_state:
+        st.session_state.chunked_data = create_chunk_df(st.session_state.data, model)
+
+    if "embeddings_upserted" not in st.session_state:
+        upsert_embeddings(st.session_state.chunked_data, index)
+        st.session_state.embeddings_upserted = True
+
     chat(model, index)
 
 if __name__ == "__main__":
